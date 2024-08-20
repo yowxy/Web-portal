f697e020337be985fc93b2e06eb8186a6b5dc112
yowxy <iklilblender@gmail.com>
2024-08-21 02:58:29 +0700

    create a CRUD authors

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use illuminate\Support\Str;
 
 
 class Author extends Model
@@ -17,6 +18,13 @@ class Author extends Model
         'avatar',
         'slug'
     ];
+
+
+    public function setNameAttribute($value){
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
 
     public function news(): HasMany{
         return $this->hasMany(ArticleNews::class);
