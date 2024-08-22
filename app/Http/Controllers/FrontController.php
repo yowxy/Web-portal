5339cd3decc16ba701fc1d95041e89b4e56e107c
yowxy <iklilblender@gmail.com>
2024-08-22 07:06:59 +0700

    show display featured and bannerAdsvertisement

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ArticleNews;
 use App\Models\Author;
+use App\Models\BannerAdvertisement;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -12,13 +13,29 @@ class FrontController extends Controller
     public function index() {
         $categories = Category::all();
         $articles = ArticleNews::with(['category'])
-            ->where('is_featured','featured')
+            ->where('is_featured','not_featured')
             ->latest()
             ->take(3)
             ->get();
 
+
+        $featured_articles = ArticleNews::with(['category'])
+            ->where('is_featured','featured')
+            ->inRandomOrder()
+            ->latest()
+            ->take(3)
+            ->get();
+
+
+        $bannerads = BannerAdvertisement::where('is_active','active')
+        ->where('type','banner')
+            ->inRandomOrder()
+            // ->take(1)
+            ->first();
+
+
         $authors = Author::all();
 
-        return view('front.index', compact('categories','articles','authors'));
+        return view('front.index', compact('categories','articles','authors', 'featured_articles', 'bannerads'));
     }
 }
